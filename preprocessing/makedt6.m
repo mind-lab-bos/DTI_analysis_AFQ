64511b0b8ee6dc507082c54c331d9e5bd6339517
Yaen <54678729+sllienaa@users.noreply.github.com>
2021-02-15 17:34:16 -0500

    Create makedt6.m

---
+++ preprocessing/makedt6.m
@@ -0,0 +1,13 @@
+% Create a cell array where each cell is the path to a data directory. In our case, sub111_1 is an example.
+sub_dirs = fileread('sub_dirs.txt');
+sub_dirs = regexp(sub_dirs, '\r\n|\r|\n', 'split');
+
+% Run the dtiMakeDt6FromFsl function to generate DT6.mat files from FSL preprocessed data 
+for ii = 1:length(sub_dirs)
+    addpath (char(strcat(sub_dirs(ii))));
+    cd (char(strcat(sub_dirs(ii))));
+    b0filename = char(strcat(sub_dirs(ii), '_dfit_S0.nii.gz'));
+    FAfilename = char(strcat(sub_dirs(ii), '_dfit_FA.nii.gz'));
+    dtiMakeDt6FromFsl([b0filename], [FAfilename]);
+    cd ..
+end
