90cb5ad25b69a8556a92c1638e55c6ccd2f92135
Yaen <54678729+sllienaa@users.noreply.github.com>
2021-02-15 18:20:24 -0500

    Update makedt6.m

--- preprocessing/makedt6.m
+++ preprocessing/makedt6.m
@@ -3,11 +3,17 @@ sub_dirs = fileread('sub_dirs.txt');
 sub_dirs = regexp(sub_dirs, '\r\n|\r|\n', 'split');
 
 % Run the dtiMakeDt6FromFsl function to generate DT6.mat files from FSL preprocessed data 
-for ii = 1:length(sub_dirs)
+for ii = 1:length(sub_dirs);
     addpath (char(strcat(sub_dirs(ii))));
     cd (char(strcat(sub_dirs(ii))));
-    b0filename = char(strcat(sub_dirs(ii), '_dfit_S0.nii.gz'));
-    FAfilename = char(strcat(sub_dirs(ii), '_dfit_FA.nii.gz'));
-    dtiMakeDt6FromFsl([b0filename], [FAfilename]);
-    cd ..
+    dt6filename = char(strcat(sub_dirs(ii), '_dfit_dt6.mat'))
+    if isfile(dt6filename)
+        warningMessage = sprintf('Warning: dt6.mat exists\n%s', dt6filename);
+        cd ..
+    else
+        b0filename = char(strcat(sub_dirs(ii), '_dfit_S0.nii.gz'));
+        FAfilename = char(strcat(sub_dirs(ii), '_dfit_FA.nii.gz'));
+        dtiMakeDt6FromFsl([b0filename], [FAfilename]);
+        cd ..
+    end
 end
