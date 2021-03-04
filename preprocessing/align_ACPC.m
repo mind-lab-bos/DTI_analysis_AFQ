8fb1bc934868dd33a33ffac2976f561fc925a9ac
Yaen <54678729+sllienaa@users.noreply.github.com>
2021-03-04 08:43:37 -0500

    Update align_ACPC.m

--- preprocessing/align_ACPC.m
+++ preprocessing/align_ACPC.m
@@ -1,7 +1,7 @@
 for ii = 1:length(sub_dirs);
     addpath (char(strcat(sub_dirs(ii))));
     cd (char(strcat(sub_dirs(ii))));
-    fileNameList = 'char(strcat(sub_dirs(ii), '1_dfit_FA.mat'))'
+    fileNameList = char(strcat(sub_dirs(ii), '1_dfit_FA.mat'))
     outFileName = 'dt6.mat'
     mrAnatAverageAcpcNifti
     %% Use the GUI to select ACPC landmarks
