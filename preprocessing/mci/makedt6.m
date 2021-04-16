d49b464b9965062cdab11cabeeecf20f1954b882
Yaen <54678729+sllienaa@users.noreply.github.com>
2021-04-15 22:41:31 -0400

    Update makedt6.m

--- preprocessing/mci/makedt6.m
+++ preprocessing/mci/makedt6.m
@@ -1,12 +1,7 @@
 for ii = 1:length(sub_dirs);
-  cd (char(strcat(sub_dirs(ii))));
-  if isfile(dt6filename)
-    warningMessage = sprintf('Warning: dt6.mat exists\n%s', dt6filename);
-    cd ..
-  else
-      %% Since data is from a Siemens scanner, make sure to set rotateBvecsWithCanXform to 1
-    dwParams = dtiInitParams('rotateBvecsWithCanXform', 1)
-	  [dt6FileName, outBaseDir] = dtiInit([],[],dwParams);
-    cd ..
-  end
+	cd (char(strcat(sub_dirs(ii))));
+	%% Since data is from a Siemens scanner, make sure to set rotateBvecsWithCanXform to 1
+	dwParams = dtiInitParams('rotateBvecsWithCanXform', 1)
+	[dt6FileName, outBaseDir] = dtiInit([],[],dwParams);
+	cd ..
 end
